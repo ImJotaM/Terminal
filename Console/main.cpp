ac964c3d91f3af378aa908f00ed262410aa6dbda
joão marcos <jo0209.letras@gmail.com>
2024-11-26 08:32:40 -0300

    Organizing Setup

--- Console/main.cpp
+++ Console/main.cpp
@@ -1,98 +1,153 @@
 #include <raylib.h>
 #include <iostream>
 
-#define BORDER_SIZE 10
+struct Screen {
 
-int window_width = 600;
-int window_height = 450;
+	int width = 0;
+	int height = 0;
 
-Vector2 windowPos = { 0, 0 };
+};
 
-struct Window_States {
+struct WindowBorder {
+
+	int size = 0;
+	bool at_right = false;
+	bool at_bottom = false;
+
+};
+
+struct WindowStates {
 
 	bool dragging = false;
 	bool resizing = false;
 
-} window_states;
+};
+
+struct WindowData {
+
+	const char* title = "";
+	int width = 0;
+	int height = 0;
+	Vector2 position = { 0, 0 };
+	ConfigFlags flags = FLAG_WINDOW_UNDECORATED;
+	int max_FPS = 60;
+
+	WindowBorder border = { 0, false, false };
+
+	WindowStates states = { false, false };
+
+	Rectangle navbar = { 0, 0, 0, 28 };
+
+};
+
+Screen screen;
+WindowData window;
 
 Vector2 dragOffset = { 0, 0 };
 
-bool at_right = false;
-bool at_bottom = false;
-
 Vector2 mouse_pos = { 0, 0 };
 
-Rectangle navbar = { 0, 0, window_width, 28 };
-
 void CheckWindowDragging() {
-	if (CheckCollisionPointRec(mouse_pos, navbar)) {
-		window_states.dragging = true;
+	if (CheckCollisionPointRec(mouse_pos, window.navbar)) {
+		window.states.dragging = true;
 		dragOffset = mouse_pos;
 	}
 }
 
 void CheckWindowResizing() {
-	if ((at_right || at_bottom) && !window_states.resizing) {
-		window_states.resizing = true;
+	if ((window.border.at_right || window.border.at_bottom) && !window.states.resizing) {
+		window.states.resizing = true;
 	}
 }
 
 void MoveWindow() {
-	if (window_states.dragging) {
+	if (window.states.dragging) {
 		Vector2 delta = { mouse_pos.x - dragOffset.x, mouse_pos.y - dragOffset.y};
-		SetWindowPosition(windowPos.x + delta.x, windowPos.y + delta.y);
+		SetWindowPosition(window.position.x + delta.x, window.position.y + delta.y);
 	}
 }
 
 void ResizeWindow() {
 
-	if (!window_states.resizing) {
-		at_right = mouse_pos.x >= window_width - BORDER_SIZE;
-		at_bottom = mouse_pos.y >= window_height - BORDER_SIZE;
+	if (!window.states.resizing) {
+		window.border.at_right = mouse_pos.x >= window.width - window.border.size;
+		window.border.at_bottom = mouse_pos.y >= window.height - window.border.size;
 	}
 
-	if (at_right && at_bottom) {
+	if (window.border.at_right && window.border.at_bottom) {
 		SetMouseCursor(MOUSE_CURSOR_RESIZE_NWSE);
 	}
-	else if (at_right) {
+	else if (window.border.at_right) {
 		SetMouseCursor(MOUSE_CURSOR_RESIZE_EW);
 	}
-	else if (at_bottom) {
+	else if (window.border.at_bottom) {
 		SetMouseCursor(MOUSE_CURSOR_RESIZE_NS);
 	}
 	else {
 		SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 	}
 
-	if (window_states.resizing) {
+	if (window.states.resizing) {
 
-		if (at_right) window_width = mouse_pos.x;
-		if (at_bottom) window_height = mouse_pos.y;
+		if (window.border.at_right) window.width = mouse_pos.x;
+		if (window.border.at_bottom) window.height = mouse_pos.y;
 
-		if (window_width <= 200) window_width = 200;
-		if (window_height <= 200) window_height = 200;
+		if (window.width <= 200) window.width = 200;
+		if (window.height <= 200) window.height = 200;
 
-		SetWindowSize(window_width, window_height);
+		SetWindowSize(window.width, window.height);
 
 	}
 
-	navbar.width = window_width;
+	window.navbar.width = window.width;
 
 }
 
 void drawNavbar() {
-	DrawRectangleRec(navbar, { 32, 32, 32, 255 });
+	
+	DrawRectangleRec(window.navbar, { 32, 32, 32, 255 });
+
+	Vector2 X_offset = { 5, 5 };
+	int X_size = 16;
+	Rectangle area = { window.width - X_size - X_offset.x , 0 + X_offset.y , X_size, X_size };
+
+	DrawLine(area.x, area.y, area.x + area.width, area.y + area.height, WHITE);
+	DrawLine(area.x, area.y + area.height, area.x + area.width, area.y, WHITE);
+
 }
 
 int main() {
+
+	window.title = "Terminal";
+
+	InitWindow(0, 0, "");
+
+	SetWindowTitle(window.title);
+
+	screen.width = GetScreenWidth();
+	screen.height = GetScreenHeight();
 	
-	SetConfigFlags(FLAG_WINDOW_UNDECORATED);
-	InitWindow(window_width, window_height, "Terminal");
-	SetTargetFPS(60);
+	window.width = screen.width / 2;
+	window.height = screen.height / 2;
+	SetWindowSize(window.width, window.height);
+
+	window.position.x = (screen.width - window.width) / 2;
+	window.position.y = (screen.height - window.height) / 2;
+	SetWindowPosition(window.position.x, window.position.y);
+
+	window.flags = FLAG_WINDOW_UNDECORATED;
+	SetWindowState(window.flags);
+
+	SetTargetFPS(window.max_FPS);
+
+	window.border.size = 10;
+	window.navbar = { 0, 0 };
+	window.navbar.width = window.width;
+	window.navbar.height = 28;
 
 	while (!WindowShouldClose()) {
 
-		windowPos = GetWindowPosition();
+		window.position = GetWindowPosition();
 		mouse_pos = GetMousePosition();
 
 		if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
@@ -103,8 +158,8 @@ int main() {
 		}
 
 		if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-			window_states.dragging = false;
-			window_states.resizing = false;
+			window.states.dragging = false;
+			window.states.resizing = false;
 		}
 
 		MoveWindow();
